f10955354ed145a4def690df73bb1e465972c2b1
huapuyu <huapuyu@a2ae3c22-c76a-8987-d788-9ad7ca98c216>
2010-10-18 14:26:02 +0000

    增加setSqlMapClientMocker方法，避免在XML文件中给DAO方法注入SqlMapClient。

--- ssh/src/dao/ibatis/Tb_userDao.java
+++ ssh/src/dao/ibatis/Tb_userDao.java
@@ -2,16 +2,27 @@ package dao.ibatis;
 
 import java.util.List;
 
+import javax.annotation.Resource;
+
 import model.test.Tb_user;
 
 import org.springframework.orm.ibatis.support.SqlMapClientDaoSupport;
 import org.springframework.stereotype.Component;
+
+import com.ibatis.sqlmap.client.SqlMapClient;
 
 import dao.interf.ITb_userDao;
 
 @Component
 public class Tb_userDao extends SqlMapClientDaoSupport implements ITb_userDao
 {
+	// 增加setSqlMapClientMocker方法，避免在XML文件中给DAO方法注入SqlMapClient。
+	@Resource
+	public void setSqlMapClientMocker(SqlMapClient sqlMapClient)
+	{
+		super.setSqlMapClient(sqlMapClient);
+	}
+
 	@Override
 	public void delete(Tb_user model)
 	{
