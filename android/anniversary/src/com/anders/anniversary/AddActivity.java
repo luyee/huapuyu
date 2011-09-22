ce1627431af21b793f746616da64f789c9a321f2
huapuyu <huapuyu@a2ae3c22-c76a-8987-d788-9ad7ca98c216>
2011-09-22 14:46:19 +0000

    1.多个Activity之间传递数据。 2.发送短信。

--- android/anniversary/src/com/anders/anniversary/AddActivity.java
+++ android/anniversary/src/com/anders/anniversary/AddActivity.java
@@ -1,19 +1,23 @@
 package com.anders.anniversary;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class AddActivity extends Activity{
+public class AddActivity extends Activity {
 
 	private TextView textView1 = null;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.add);
-		textView1 = (TextView)findViewById(R.id.textView1);
-		textView1.setText(R.string.add_textView1);
+		Intent intent = getIntent();
+		String value = intent.getStringExtra("name");
+		textView1 = (TextView) findViewById(R.id.textView1);
+		// textView1.setText(R.string.add_textView1);
+		textView1.setText(value);
 	}
 
 }
--- android/anniversary/src/com/anders/anniversary/MainActivity.java
+++ android/anniversary/src/com/anders/anniversary/MainActivity.java
@@ -2,30 +2,47 @@ package com.anders.anniversary;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class MainActivity extends Activity {
-	
+
 	private Button button1 = null;
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        button1 = (Button)findViewById(R.id.button1);
-        button1.setText(R.string.add_button1);
-        button1.setOnClickListener(new Button1Listener());
-    }
-    
-    class Button1Listener implements OnClickListener{
+	private Button button2 = null;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		button1 = (Button) findViewById(R.id.button1);
+		button1.setText(R.string.add_button1);
+		button1.setOnClickListener(new Button1Listener());
+
+		button2 = (Button) findViewById(R.id.button2);
+		button2.setText(R.string.sendmsg_button2);
+		button2.setOnClickListener(new Button2Listener());
+	}
+
+	class Button1Listener implements OnClickListener {
 		@Override
 		public void onClick(View v) {
 			Intent intent = new Intent();
+			intent.putExtra("name", "zhuzhen");
 			intent.setClass(MainActivity.this, AddActivity.class);
 			MainActivity.this.startActivity(intent);
 		}
-    }
+	}
+
+	class Button2Listener implements OnClickListener {
+		@Override
+		public void onClick(View v) {
+			Uri uri = Uri.parse("smsto://0800000123");
+			Intent intent = new Intent(Intent.ACTION_SENDTO, uri);
+			intent.putExtra("sms_body", "妹妹我爱你！");
+			startActivity(intent);
+		}
+	}
 }
