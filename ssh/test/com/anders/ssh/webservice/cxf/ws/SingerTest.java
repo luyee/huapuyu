518d8cac57ad41ea908746085554c536275a74be
huapuyu <huapuyu@a2ae3c22-c76a-8987-d788-9ad7ca98c216>
2011-12-13 08:44:11 +0000

    由WS和RS服务端没启动，所以WS和RS客户端代理Bean无法创建，造成单元测试跑不通，因此分离单元测试的Spring配置文件

--- ssh/test/com/anders/ssh/webservice/cxf/ws/SingerTest.java
+++ ssh/test/com/anders/ssh/webservice/cxf/ws/SingerTest.java
@@ -11,7 +11,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "classpath:spring-test.xml" })
+@ContextConfiguration(locations = { "classpath:spring-remote-test.xml" })
 public class SingerTest {
 	@Autowired
 	private SingerService singerService;
