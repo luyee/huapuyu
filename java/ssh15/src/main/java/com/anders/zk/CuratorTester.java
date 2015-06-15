800c485889e001b9f7ab1aa8ece63d2d853c9af1
huapuyu <huapuyu@gmail.com>
2015-06-15 23:01:44 +0800

    提交zk代码

--- java/ssh15/src/main/java/com/anders/zk/CuratorTester.java
+++ java/ssh15/src/main/java/com/anders/zk/CuratorTester.java
@@ -1,20 +1,55 @@
 package com.anders.zk;
 
+import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.apache.curator.retry.RetryNTimes;
+import org.apache.curator.retry.RetryOneTime;
+import org.apache.curator.retry.RetryUntilElapsed;
+import org.junit.Test;
 
 public class CuratorTester {
-	public static void main(String[] args) {
-//		String path = "/test_path";  
-	     CuratorFramework client = CuratorFrameworkFactory.builder().
-	    		 connectString("e4430:2181").
-	    		 //namespace("/").
-	    		 retryPolicy(new RetryNTimes(Integer.MAX_VALUE, 1000)).
-	    		 connectionTimeoutMs(5000).
-	    		 build();  
-	     client.start();  
-	     
-	     System.out.println(client.getState());
+	// 查看路径状态
+	@Test
+	public void test1() {
+		RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
+		retryPolicy = new RetryNTimes(3, 1000);
+		retryPolicy = new RetryOneTime(1000);
+		retryPolicy = new RetryUntilElapsed(1000, 1000);
+
+		CuratorFramework client = CuratorFrameworkFactory.builder()
+				.connectString("anders1:2181,anders2:2181,anders3:2181")
+				.namespace("anders_test").retryPolicy(retryPolicy)
+				.connectionTimeoutMs(5000).sessionTimeoutMs(5000).build();
+		client.start();
+
+		System.out.println(client.getState());
+	}
+
+	// 创建路径，指定namespace
+	@Test
+	public void test2() throws Exception {
+		RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
+		CuratorFramework client = CuratorFrameworkFactory.builder()
+				.connectString("anders1:2181,anders2:2181,anders3:2181")
+				.namespace("anders_test").retryPolicy(retryPolicy)
+				.connectionTimeoutMs(5000).sessionTimeoutMs(5000).build();
+		client.start();
+		client.create().creatingParentsIfNeeded()
+				.forPath("/test1", "helloworld".getBytes());
+	}
+
+	// 创建路径，不指定namespace
+	@Test
+	public void test3() throws Exception {
+		RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
+		CuratorFramework client = CuratorFrameworkFactory.builder()
+				.connectString("anders1:2181,anders2:2181,anders3:2181")
+				.retryPolicy(retryPolicy).connectionTimeoutMs(5000)
+				.sessionTimeoutMs(5000).build();
+		client.start();
+		client.create().creatingParentsIfNeeded()
+				.forPath("/anders_test/test2", "sayhello".getBytes());
 	}
 }
