622789d23fc670db78962475a56f930896f8f88b
朱振 <anders.zhu@vipshop.com>
2015-06-19 12:07:36 +0800

    提交aio测试代码

--- java/experiment/src/main/java/com/anders/experiment/network/aio/AIOTest.java
+++ java/experiment/src/main/java/com/anders/experiment/network/aio/AIOTest.java
@@ -5,19 +5,22 @@ import java.util.concurrent.ExecutionException;
 
 import org.junit.Test;
 
+// 先启动server，后启动client
 public class AIOTest {  
     
     @Test  
     public void testServer() throws IOException, InterruptedException {  
-        SimpleServer server = new SimpleServer(1234);  
+        /*SimpleServer server = */new SimpleServer(1234);  
+        System.out.println("server is running...");
           
-        Thread.sleep(10000);  
+        Thread.sleep(60000);  
     }  
       
     @Test  
     public void testClient() throws IOException, InterruptedException, ExecutionException {  
-        SimpleClient client = new SimpleClient("localhost", 7788);  
-        client.write((byte) 11);  
+        SimpleClient client = new SimpleClient("localhost", 1234);  
+        client.write((byte) 11); 
+        System.out.println("send : " + 11);
     }  
   
 }  
