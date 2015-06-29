163ac6729738d1e41f649b8f3f145c006c6e843d
huapuyu <huapuyu@gmail.com>
2015-06-30 01:28:04 +0800

    commit

--- java/ssh15/src/main/java/com/anders/es/ClientTest.java
+++ java/ssh15/src/main/java/com/anders/es/ClientTest.java
@@ -1,27 +1,26 @@
 package com.anders.es;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
-import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.ImmutableSettings;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.junit.Test;
 
 public class ClientTest {
-	
-//	@Configuration
-//	@EnableElasticsearchRepositories(basePackages = "org/springframework/data/elasticsearch/repositories")
-//	static class Config {
-//
-//	    @Bean
-//	    public ElasticsearchOperations elasticsearchTemplate() {
-//	        return new ElasticsearchTemplate(nodeBuilder().local(true).node().client());
-//	    }
-//	}
-//	
- public void test1() {
-	 ElasticsearchTemplate elasticsearchTemplate;
-//	 elasticsearchTemplate.
- }
+
+	@Test
+	public void test1() {
+		Client client = new TransportClient().addTransportAddress(new InetSocketTransportAddress("lu1", 9300))
+				.addTransportAddress(new InetSocketTransportAddress("lu2", 9300))
+				.addTransportAddress(new InetSocketTransportAddress("lu3", 9300));
+		client.close();
+	}
+
+	@Test
+	public void test2() {
+		Settings settings = ImmutableSettings.settingsBuilder().put("cluster.name", "anders").build();
+		Client client = new TransportClient(settings);
+		client.close();
+	}
 }
-
-
