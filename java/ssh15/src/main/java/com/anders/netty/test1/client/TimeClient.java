c0d83cb81b06aff1f00cebb2b3aff545ba1ab8cb
huapuyu <huapuyu@gmail.com>
2015-10-02 16:03:51 +0800

    commit netty code

---
+++ java/ssh15/src/main/java/com/anders/netty/test1/client/TimeClient.java
@@ -0,0 +1,50 @@
+package com.anders.netty.test1.client;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+
+public class TimeClient {
+	public void connect(int port, String host) throws Exception {
+		EventLoopGroup group = new NioEventLoopGroup();
+		try {
+			Bootstrap bootstrap = new Bootstrap();
+			bootstrap.group(group).channel(NioSocketChannel.class)
+					.option(ChannelOption.TCP_NODELAY, true)
+					.handler(new ChildChannelHandler());
+
+			ChannelFuture cf = bootstrap.connect(host, port).sync();
+			cf.channel().closeFuture().sync();
+		} finally {
+			group.shutdownGracefully();
+		}
+	}
+
+	private class ChildChannelHandler
+			extends ChannelInitializer<SocketChannel> {
+		@Override
+		protected void initChannel(SocketChannel arg0) throws Exception {
+			arg0.pipeline().addLast(new LineBasedFrameDecoder(1024));
+			arg0.pipeline().addLast(new StringDecoder());
+			arg0.pipeline().addLast(new TimeClientHandler());
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		// 13 CR 回车
+		// 10 LF 换行
+		// byte[] bytes = System.getProperty("line.separator").getBytes();
+		// for (byte b : bytes) {
+		// System.out.println(b);
+		// }
+
+		new TimeClient().connect(8080, "127.0.0.1");
+	}
+}
---
+++ java/ssh15/src/main/java/com/anders/netty/test1/server/TimeServerHandler.java
@@ -0,0 +1,45 @@
+package com.anders.netty.test1.server;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+
+public class TimeServerHandler extends ChannelHandlerAdapter {
+
+	private int counter;
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause)
+			throws Exception {
+		ctx.close();
+	}
+
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg)
+			throws Exception {
+		// ByteBuf buf = (ByteBuf) msg;
+		// byte[] request = new byte[buf.readableBytes()];
+		// buf.readBytes(request);
+		// String body = new String(request, "UTF-8");
+		// String body = new String(request, "UTF-8").substring(0,
+		// request.length - System.getProperty("line.separator").length());
+		String body = (String) msg;
+		System.out.println(String.format(
+				"The time server receive order : %s; counter : %d", body,
+				++counter));
+		String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body)
+				? new java.util.Date(System.currentTimeMillis()).toString()
+				: "BAD ORDER";
+		currentTime += System.getProperty("line.separator");
+		ByteBuf response = Unpooled.copiedBuffer(currentTime.getBytes());
+		// ctx.write(response);
+		ctx.writeAndFlush(response);
+	}
+
+	@Override
+	public void channelReadComplete(ChannelHandlerContext ctx)
+			throws Exception {
+		ctx.flush();
+	}
+}
