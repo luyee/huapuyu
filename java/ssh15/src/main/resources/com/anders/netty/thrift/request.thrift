0a5845de6cef11018c582538ce58dd3f6af83b89
huapuyu <huapuyu@gmail.com>
2015-10-05 10:12:42 +0800

    commit thrift code

---
+++ java/ssh15/src/main/resources/com/anders/netty/thrift/request.thrift
@@ -0,0 +1,12 @@
+namespace java com.anders.netty.thrift.api  
+  
+struct Request {  
+1:i32 id;  
+2:string name;  
+3:string address;  
+}  
+  
+//service IndexNewsOperatorServices {  
+//bool indexNews(1:NewsModel indexNews),  
+//bool deleteArtificiallyNews(1:i32 id )  
+//}
---
+++ java/ssh15/src/main/resources/com/anders/netty/thrift/response.thrift
@@ -0,0 +1,7 @@
+namespace java com.anders.netty.thrift.api  
+  
+struct Response {  
+1:i32 id;  
+2:bool ok;  
+3:string desc;  
+}  
