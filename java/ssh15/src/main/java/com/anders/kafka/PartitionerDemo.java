45b98b999949f04972771af2d06cad1b61b42c04
huapuyu <huapuyu@gmail.com>
2015-10-22 14:27:03 +0800

    commit code

--- java/ssh15/src/main/java/com/anders/kafka/PartitionerDemo.java
+++ java/ssh15/src/main/java/com/anders/kafka/PartitionerDemo.java
@@ -11,17 +11,21 @@ public class PartitionerDemo implements Partitioner {
 	@Override
 	public int partition(Object obj, int numPartitions) {
 		int partition = 0;
-		if (obj instanceof String) {
-			String key = (String) obj;
-			int offset = key.lastIndexOf('.');
-			if (offset > 0) {
-				partition = Integer.parseInt(key.substring(offset + 1)) % numPartitions;
-			}
-		} else {
-			partition = obj.toString().length() % numPartitions;
-		}
+		// if (obj instanceof String) {
+		// String key = (String) obj;
+		// int offset = key.lastIndexOf('.');
+		// if (offset > 0) {
+		// partition = Integer.parseInt(key.substring(offset + 1)) %
+		// numPartitions;
+		// }
+		// } else {
+		// partition = obj.toString().length() % numPartitions;
+		// }
 
-		return partition;
+		String key = (String) obj;
+		int i = Integer.parseInt(key);
+
+		return i % numPartitions;
 	}
 
 }
