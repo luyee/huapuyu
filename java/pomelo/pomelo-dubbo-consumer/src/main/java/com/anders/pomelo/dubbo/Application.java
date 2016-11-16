76bffba9c356bad3eceaf92ba9145e1232839887
huapuyu <huapuyu@qq.com>
2016-11-16 19:32:46 +0800

    commit code

--- java/pomelo/pomelo-dubbo-consumer/src/main/java/com/anders/pomelo/dubbo/Application.java
+++ java/pomelo/pomelo-dubbo-consumer/src/main/java/com/anders/pomelo/dubbo/Application.java
@@ -4,20 +4,26 @@ import java.io.IOException;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ImportResource;
 
+import com.anders.pomelo.dubbo.service.UserService;
+
 @SpringBootApplication
-@ImportResource({ "classpath:dubbo-provider.xml" })
+@ImportResource({ "classpath:dubbo-consumer.xml" })
 public class Application {
 
 	// private static final Logger LOGGER = LoggerFactory
 	// .getLogger(Application.class);
 
 	public static void main(String[] args) throws IOException, InterruptedException {
-		// SpringApplication.run(Application.class, args);
-		// com.alibaba.dubbo.container.Main.main(args);
 
-		new SpringApplicationBuilder().sources(Application.class).web(false).run(args);
+		ApplicationContext ctx = new SpringApplicationBuilder().sources(Application.class).web(false).run(args);
+
+		UserService userService = ctx.getBean(UserService.class);
+		for (int i = 0; i < 100; i++) {
+			System.out.println(userService.sayFuck("zhuzhen"));
+		}
 
 		System.in.read();
 	}
--- java/pomelo/pomelo-dubbo-provider1/src/main/java/com/anders/pomelo/dubbo/service/impl/UserServiceImpl.java
+++ java/pomelo/pomelo-dubbo-provider1/src/main/java/com/anders/pomelo/dubbo/service/impl/UserServiceImpl.java
@@ -17,35 +17,31 @@ public class UserServiceImpl implements UserService {
 
 	@Override
 	public String sayFuck(String name) {
-		System.out.println("[" + new SimpleDateFormat("HH:mm:ss").format(new Date()) + "] Fuck " + name + ",request from consumer:"
-				+ RpcContext.getContext().getRemoteAddress());
-		return "Fuck " + name + ",response form provider:" + RpcContext.getContext().getLocalAddress();
+		System.out.println("[" + new SimpleDateFormat("HH:mm:ss").format(new Date()) + "] Fuck " + name + ", request from consumer : " + RpcContext.getContext().getRemoteAddress());
+		return "Fuck " + name + ", response from provider : " + RpcContext.getContext().getLocalAddress();
 	}
 
 	@Override
 	public String sayHello(User user) {
-		System.out.println("[" + new SimpleDateFormat("HH:mm:ss").format(new Date()) + "] Hello " + user.getName() + ",request from consumer:"
-				+ RpcContext.getContext().getRemoteAddress());
-		return "Hello " + user.getId() + ":" + user.getName() + ",response form provider:" + RpcContext.getContext().getLocalAddress();
+		System.out.println("[" + new SimpleDateFormat("HH:mm:ss").format(new Date()) + "] Hello " + user.getName() + ", request from consumer : " + RpcContext.getContext().getRemoteAddress());
+		return "Hello " + user.getId() + "-" + user.getName() + ", response from provider : " + RpcContext.getContext().getLocalAddress();
 	}
 
 	@Override
 	public String sayTimeout(String name) {
-		System.out.println("[" + new SimpleDateFormat("HH:mm:ss").format(new Date()) + "] Timeout " + name + ",request from consumer:"
-				+ RpcContext.getContext().getRemoteAddress());
+		System.out.println("[" + new SimpleDateFormat("HH:mm:ss").format(new Date()) + "] Timeout " + name + ", request from consumer : " + RpcContext.getContext().getRemoteAddress());
 
 		try {
 			Thread.sleep(3000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		return "Timeout " + name + ",response form provider:" + RpcContext.getContext().getLocalAddress();
+		return "Timeout " + name + ", response from provider : " + RpcContext.getContext().getLocalAddress();
 	}
 
 	@Override
 	public String sayTimeoutJO(String name) {
-		System.out.println("[" + new SimpleDateFormat("HH:mm:ss").format(new Date()) + "] Timeout " + name + ",request from consumer:"
-				+ RpcContext.getContext().getRemoteAddress());
+		System.out.println("[" + new SimpleDateFormat("HH:mm:ss").format(new Date()) + "] Timeout " + name + ", request from consumer : " + RpcContext.getContext().getRemoteAddress());
 
 		if ((count.getAndIncrement() % 2) == 0) {
 		} else {
@@ -55,7 +51,7 @@ public class UserServiceImpl implements UserService {
 				e.printStackTrace();
 			}
 		}
-		return "Timeout " + name + ",response form provider:" + RpcContext.getContext().getLocalAddress();
+		return "Timeout " + name + ", response from provider : " + RpcContext.getContext().getLocalAddress();
 	}
 
 }
