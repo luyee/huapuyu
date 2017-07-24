4f48bb22d839055109dd1e09a6f5c2680544fedd
ander <ander@172.16.101.19>
2017-07-24 19:32:30 +0800

    commit

---
+++ java/springboot/springboot-databus-server/src/test/java/com/anders/pomelo/databus/GenMyBatisCodeTest.java
@@ -0,0 +1,32 @@
+package com.anders.pomelo.databus;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+public class GenMyBatisCodeTest {
+
+	@Test
+	public void test() throws IOException, XMLParserException, InvalidConfigurationException, SQLException, InterruptedException {
+		List<String> warnings = new ArrayList<String>();
+		boolean overwrite = true;
+		File configFile = new File(
+				"C:/Users/ander/git/huapuyu/java/springboot/springboot-databus-server/src/test/resources/generatorConfig.xml");
+		ConfigurationParser cp = new ConfigurationParser(warnings);
+		Configuration config = cp.parseConfiguration(configFile);
+		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+		MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+		myBatisGenerator.generate(null);
+	}
+
+}
