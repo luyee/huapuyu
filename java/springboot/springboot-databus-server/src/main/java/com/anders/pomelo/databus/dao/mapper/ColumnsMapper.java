77a4d9e181208d42415bd93563c859a6e82ed6a2
ander <ander@172.16.101.18>
2017-07-25 16:07:38 +0800

    commit

---
+++ java/springboot/springboot-databus-server/src/main/java/com/anders/pomelo/databus/dao/mapper/ColumnsMapper.java
@@ -0,0 +1,10 @@
+package com.anders.pomelo.databus.dao.mapper;
+
+import java.util.List;
+
+import com.anders.pomelo.databus.dao.bo.Columns;
+
+public interface ColumnsMapper {
+   
+    List<Columns> selectByTableSchema(String tableSchema);
+}
---
+++ java/springboot/springboot-databus-server/src/test/java/com/anders/pomelo/databus/dao/ColumnsTest.java
@@ -0,0 +1,38 @@
+package com.anders.pomelo.databus.dao;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.anders.pomelo.databus.dao.bo.Columns;
+import com.anders.pomelo.databus.dao.mapper.ColumnsMapper;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:spring/applicationContext-dao-test.xml" })
+public class ColumnsTest {
+
+	@Resource(name = "columnsMapper")
+	private ColumnsMapper columnsMapper;
+
+	@Test
+	public void test() {
+		List<Columns> columnsList = columnsMapper.selectByTableSchema("eif_test");
+		for (Columns columns: columnsList) {
+			System.out.println(columns.getCharacterSetName());
+			System.out.println(columns.getColumnKey());
+			System.out.println(columns.getColumnName());
+			System.out.println(columns.getColumnType());
+			System.out.println(columns.getDataType());
+			System.out.println(columns.getTableName());
+			System.out.println(columns.getDatetimePrecision());
+			System.out.println(columns.getOrdinalPosition());
+			System.out.println("*************************************");
+		}
+	}
+
+}
