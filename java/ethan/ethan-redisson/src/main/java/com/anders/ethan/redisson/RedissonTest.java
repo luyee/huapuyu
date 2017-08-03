aaf131b0dd63ea578e172ec2d691730d1aac3751
huapuyu <huapuyu@qq.com>
2017-08-03 17:00:35 +0800

    commit

--- java/ethan/ethan-redisson/src/main/java/com/anders/ethan/redisson/RedissonTest.java
+++ java/ethan/ethan-redisson/src/main/java/com/anders/ethan/redisson/RedissonTest.java
@@ -1,6 +1,7 @@
 package com.anders.ethan.redisson;
 
 import org.redisson.Redisson;
+import org.redisson.api.RBucket;
 import org.redisson.api.RMap;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
@@ -15,18 +16,30 @@ public class RedissonTest {
 				.addNodeAddress("redis://192.168.56.106:6379");
 
 		RedissonClient redisson = Redisson.create(config);
-		RMap<String, String> map = redisson.getMap("anyMap");
+		// RMap<String, String> map = redisson.getMap("anyMap");
 		// map.put("zhuzhen", "guolili");
 		// map.fastPut("guolili", "helloworld");
 
-		int i = 0;
-		while (true) {
+		// int i = 0;
+		// while (true) {
+		// try {
+		// map.put(i++ + "", i++ + "");
+		// // map.get((i - 2) + "");
+		// System.out.println(map.get((i - 2) + ""));
+		// } catch (Throwable e) {
+		// System.out.println(e.getMessage());
+		// }
+		// }
+
+		String keyname = "";
+		for (int i = 0; i < 500000; i++) {
 			try {
-				map.put(i++ + "", i++ + "");
-				// map.get((i - 2) + "");
-				System.out.println(map.get((i - 2) + ""));
+				keyname = "keys" + i;
+				RBucket<String> bucket = redisson.getBucket(keyname);
+				bucket.set(keyname);
+				// System.out.println(bucket.get());
 			} catch (Throwable e) {
-				System.out.println(e.getMessage());
+				System.out.println(keyname + " ******** " + e.getMessage());
 			}
 		}
 	}
